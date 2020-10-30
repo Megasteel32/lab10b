c7382f63e4d8488cf6735f8e6fe0a23f3ce7c946
Megasteel32 <megasteel32@gmail.com>
2020-10-30 15:27:45 -0500

    pp

---
+++ program1.py
@@ -0,0 +1,15 @@
+with open("Celsius.dat", "r") as celcius:
+    noblanks = []
+    for line in celcius:
+        noblanks.append(line.splitlines())
+
+converted = []
+for x in range(len(noblanks)):
+    noblanks[x] = float(noblanks[x][0])
+for x in range(len(noblanks)):
+    converted.append(noblanks[x] * (9/5) + 32)
+print(converted)
+
+with open("Fahrenheit.dat", "w") as fahrenheit:
+    for x in converted:
+        fahrenheit.write(str(x) + "\n")
