23e0931629c2ee96c833da9d5418e95eb94d9dcf
Megasteel32 <megasteel32@gmail.com>
2020-10-31 06:13:25 -0500

    pp

--- program2.py
+++ program2.py
@@ -1,10 +1,18 @@
-# loan_amount = float(input("What is the amount of the loan? "))
-# interest_rate = float(input("What is the interest rate? "))
-# monthly_payment = float(input("What is the monthly payment? "))
+loan_amount = float(input("What is the amount of the loan? "))
+interest_rate = float(input("What is the interest rate? ")) / 100
+monthly_payment = float(input("What is the monthly payment? "))
 file_name = input("What is the name you would like to call the file? ")
+interest = 0
+month = 0
 
 file_name = file_name + ".csv"
-print(file_name)
 
 with open(file_name, "w") as file:
-    file.write("bruh")
+    file.write("Month, Loan Amount, Interest\n")
+    file.write("0, {}, 0\n".format(loan_amount))
+    while loan_amount >= 0 and month <= 30:
+        month += 1
+        interest = loan_amount * (interest_rate * (1 / 12))
+        loan_amount = loan_amount + interest - monthly_payment
+        if loan_amount >= 0 and month <= 30:
+            file.write("{}, {}, {}\n".format(month, round(loan_amount,2), round(interest,2)))
